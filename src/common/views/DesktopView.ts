8272fe5f281fa8e199e705b08d2908c5ed25a44e
Jens Herman <jens.herman@grommunio.com>
2024-04-12 15:52:30 +0200

    Added Files- & Meet-Tab, improved tab urls

--- src/common/views/DesktopView.ts
+++ src/common/views/DesktopView.ts
@@ -1,6 +1,7 @@
 /* eslint-disable header/header */
 // Copyright (c) 2020-present grommunio GmbH. All Rights Reserved.
 // See LICENSE.txt for license information.
+import {getFormattedPathName} from 'common/utils/url';
 
 import BaseView from './BaseView';
 import type {ViewType} from './View';
@@ -8,7 +9,7 @@ import {TAB_DESKTOP} from './View';
 
 export default class DesktopView extends BaseView {
     get url(): URL {
-        return new URL('https://mail.grommunio.com');
+        return new URL(`${this.server.url.origin}${getFormattedPathName(this.server.url.pathname)}web`);
     }
 
     get type(): ViewType {
--- src/common/views/MeetView.ts
+++ src/common/views/MeetView.ts
@@ -1,23 +1,22 @@
 /* eslint-disable header/header */
 // Copyright (c) 2020-present grommunio GmbH. All Rights Reserved.
-// Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 import {getFormattedPathName} from 'common/utils/url';
 
 import BaseView from './BaseView';
 import type {ViewType} from './View';
-import {TAB_MESSAGING} from './View';
+import {TAB_MEET} from './View';
 
-export default class MessagingView extends BaseView {
+export default class MeetView extends BaseView {
     get url(): URL {
-        return new URL(`${this.server.url.origin}${getFormattedPathName(this.server.url.pathname)}chat`);
+        return new URL(`${this.server.url.origin}${getFormattedPathName(this.server.url.pathname)}meet`);
     }
 
     get type(): ViewType {
-        return TAB_MESSAGING;
+        return TAB_MEET;
     }
 
     get shouldNotify(): boolean {
-        return true;
+        return false;
     }
 }
